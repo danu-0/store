f4c1216b3457b9476e7ad34de7581b1f9d561c85
Gylmynnn <muhamadgilman999@gmail.com>
2025-01-22 08:21:04 +0800

    feat : menambahkan logging

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -40,6 +40,7 @@ class Kernel extends HttpKernel
 
         'api' => [
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+            \App\Http\Middleware\LogAPI::class,
             \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
@@ -67,5 +68,5 @@ class Kernel extends HttpKernel
     protected $routeMiddleware = [
         'role' => \App\Http\Middleware\RoleMiddleware::class,
     ];
-    
+
 }
